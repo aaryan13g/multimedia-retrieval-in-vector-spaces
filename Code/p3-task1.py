1b1a8823611d9481c4551e3d0982c1bf0465600d
pranav7557 <pkatariya99@gmail.com>
2021-11-11 13:55:46 -0700

    Task1 updated

--- Code/p3-task1.py
+++ Code/p3-task1.py
@@ -1,3 +1,11 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from pandas_ml import ConfusionMatrix
+
+
 def get_input():
     print("Enter space-separated values of 'train folder', 'feature model', 'k':")
     train_folder, feature_model, k = input().split(" ")
@@ -18,3 +26,37 @@ if __name__ == "__main__":
     pred_labels = predict(model, test_matrix)
     compute_and_print_outputs(true_labels, pred_labels)
     print("Task Completed!")
+
+def train_classifier(train_matrix, labels, classifier):
+    classifier=classifier_function_name()
+    model=classifier.fit(train_matrix,labels)
+    return model
+
+def predict(model, test_matrix):
+    labels=model.predict(test_matrix)
+    return labels
+
+def compute_and_print_outputs(true_labels, pred_labels):
+    print("The true labels are :\n")
+    print(true_labels)
+    print("The predicted labels are :\n")
+    print(pred_labels)
+    print("The accuracy is :\n")
+    print(accuracy_score(true_labels, pred_labels))
+    
+    cnf_matrix = confusion_matrix(true_labels,pred_labels)  #sklearn function
+        
+    FP = cnf_matrix.sum(axis=0) - np.diag(cnf_matrix)  
+    FN = cnf_matrix.sum(axis=1) - np.diag(cnf_matrix)
+    TP = np.diag(cnf_matrix)
+    TN = cnf_matrix.sum() - (FP + FN + TP)
+
+    FP = FP.astype(float)
+    FN = FN.astype(float)
+    TP = TP.astype(float)
+    TN = TN.astype(float)
+    
+    FPR = FP/(FP+TN)       #This is the false positive rate
+    FNR = FN/(TP+FN)        #This is the misses
+    
+    # cnf_matrix.print_stats()
