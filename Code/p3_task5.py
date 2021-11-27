80a9659273437bae7f374ccb64ea052f23544f5d
pranav7557 <pkatariya99@gmail.com>
2021-11-27 02:23:03 -0700

    Task 5 almost there

--- Code/p3_task5.py
+++ Code/p3_task5.py
@@ -7,6 +7,8 @@ import itertools
 import matplotlib.pyplot as plt
 import numpy as np
 from Phase2.task5 import *
+from scipy.spatial import distance
+
 
 def extract_features_for_new_image(image_path):
     image = Image.open(image_path)
@@ -20,6 +22,7 @@ def extract_features_for_new_image(image_path):
     return new_document
 
 hash_table = dict()
+
 def create_hash(results):
     for i in range(len(results)):
         hash_table.setdefault(results[i],[]).append(i+1)
@@ -27,7 +30,8 @@ def create_hash(results):
     for keys in hash_table:
         if len(hash_table[keys])>1:
             print(hash_table[keys])
-    
+    return hash_table
+
 def decimalToBinary(n):
     return bin(n).replace("0b", "")
     
@@ -42,7 +46,7 @@ def create_VA(data_matrix,bits):
     x = np.arange((pow(2,bits))+1)
     # print("X: ",x)
     l=np.true_divide(x,(pow(2,bits)))
-    # print("L: ",l)
+    print("L: ",l)
     k=0
     results=[]
    
@@ -68,10 +72,8 @@ def create_VA(data_matrix,bits):
         print(len(data_matrix[i]))       
         print(len(results[i]))
     print(results)
-    
     print("----------------------------------------------------------------------------")
-    create_hash(results)
-    return results
+    return results, l
 
 def create_VA_Query(data_matrix,bits):
     x = np.arange((pow(2,bits))+1)
@@ -99,12 +101,12 @@ def create_VA_Query(data_matrix,bits):
 
 
 def get_input():
-    print("Enter space-separated values of 'image','bits','folder of images':")
-    image,bits, folder = input().split(" ") #Can take feature model as input too here.
+    print("Enter space-separated values of 'image','bits','folder of images','t':")
+    image,bits, folder,t = input().split(" ") #Can take feature model as input too here.
     doc=extract_features_for_new_image("../images/"+image)
     image_dm=doc['elbp']
     image_dm = np.array(image_dm)
-    return int(bits), folder, image_dm
+    return int(bits), folder, image_dm,int(t)
 
 def create_data_matrix(folder, feature_model):
     client = pymongo.MongoClient("mongodb+srv://mwdbuser:mwdbpassword@cluster0.u1c4q.mongodb.net/Phase2?retryWrites=true&w=majority")
@@ -129,9 +131,88 @@ def create_data_matrix(folder, feature_model):
     print("Shape of data matrix: ", data_matrix.shape)
     return data_matrix
 
+    
+def get_bounds(vq,ri,p,bits):
+    l = []
+    imgLen = len(vq)
+    res = 0
+    rq= create_VA_Query(vq,bits)
+    rq = [rq[i:i+bits] for i in range(0, len(rq), bits)]
+    for i in range(len(rq)):
+        rq[i]=int(rq[i],2)
+    #rq = list(map(int, rq))
+    
+    
+    print(p)
+    print(imgLen)
+    print(rq)
+    print(len(ri))
+    print(len(rq))
+    for j in range(0,imgLen):
+        if(ri[j]<rq[j]):
+            l.append(vq[j] - p[ri[j]+1])
+        elif(ri[j] == rq[j]):
+            l.append(0)
+        else:
+            l.append(p[ri[j]] - vq[j])
+    for i in range(0,len(l)):
+        res = res+(l[i]**3)
+    return res**(1/3)    
+
+def InitCandidate(n,dst):
+    for i in range(0,n):
+        dst[i] = float('inf')
+    return float('inf'),dst
+
+def Candidate(d,i,n,dst,ans):
+    if(d<dst[n]):
+        dst[n] = d
+        ans[n] = i
+        temp = []
+        sort_vals = np.argsort(dst)
+        dst.sort()
+        for i in range(0,len(sort_vals)):
+            temp.append(ans[sort_vals[i]])
+        ans = temp
+    return dst[n],ans
+
+def va_ssa(vq,vi,n,b):
+    dst = np.zeros((n))
+    count = 0
+    d,dst = InitCandidate(n,dst)
+    ans = np.zeros((n))
+    # modData,numBits,p = va_gen(vi,b)
+    
+    for j in range(len(data_matrix)):
+        chunks = [res[j][i:i+bits] for i in range(0, len(res[j]), bits)]
+        temp=[]   
+        for i in range(len(chunks)):
+            temp.append(int(chunks[i],2))    
+        li=get_bounds(image_dm,temp,p,bits)
+        if(li<d):
+            count = count+1
+            d,ans = Candidate(distance.minkowski(vq,vi[i],3),i,n-1,dst,ans)
+    
+    
 if __name__ == "__main__":
-    bits, folder ,image_dm= get_input()
+    bits, folder ,image_dm,t= get_input()
     data_matrix = create_data_matrix(folder, feature_model = "elbp")
-    res = create_VA(data_matrix,bits)
-    img= create_VA_Query(image_dm,bits)
+    res,p = create_VA(data_matrix,bits)
+    # img= create_VA_Query(image_dm,bits)
+    hashed=create_hash(res)
+    li=[]
+    
+    
+    # for j in range(len(data_matrix)):
+    #     chunks = [res[j][i:i+bits] for i in range(0, len(res[j]), bits)]
+    #     temp=[]   
+    #     for i in range(len(chunks)):
+    #         temp.append(int(chunks[i],2))    
+    #     li.append(get_bounds(image_dm,temp,p,bits))
+        #chunks = list(map(int, chunks))
+    
+    
+    # print(li)
+    
+    
     
